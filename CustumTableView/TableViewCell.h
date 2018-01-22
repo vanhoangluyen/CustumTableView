cd11987be2f22fc2f3195175c16ee973848fe67f
LuyenBG <vanhoangluyen@gmail.com>
2018-01-22 17:26:26 +0700

    init commit

--- CustumTableView/TableViewCell.h
+++ CustumTableView/TableViewCell.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewCell.h
 //  CustumTableView
 //
 //  Created by LuyenBG on 1/22/18.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
-
+@interface TableViewCell : UITableViewCell
 
 @end
-
--- CustumTableView/ViewController.m
+++ CustumTableView/ViewController.m
@@ -7,15 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "TableViewCell.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray * arrayNumber;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.arrayNumber =  [[NSArray alloc] initWithObjects: @1,@2,@3,@4,@5,@6,@7, nil ];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +27,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return  self.arrayNumber.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", self.arrayNumber[indexPath.row]];;
+    return cell;
+}
 
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
 
 @end
